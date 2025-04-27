1527e1b4de2bd3a5514814f087c9829c7232e367
Annareddy Sanjana <sanjana.12220788@lpu.in>
2025-04-27 12:48:49 +0530

    number of dimensions

---
+++ ndimfunction.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+arr_1d=np.array([2,3,45])
+print(arr_1d.ndim)
+
+arr= np.array([[1,2,3],[2,3,4],[5,3,8],[4,2,7]])
+print(arr.ndim)
+
+arr_3d=np.array([[[2,3],[35,78],[2,8],[3,0],[1,2]]])
+print(arr_3d.ndim)
