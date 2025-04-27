eacc7a76c9d9055894a3829c8d805f3e7bfc005b
Annareddy Sanjana <sanjana.12220788@lpu.in>
2025-04-27 13:40:13 +0530

    min, max, sum, var, std, mean

---
+++ Aggregatefunctions.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+arr = ([2,5,3,4,7,4,8])
+sum=np.sum(arr)
+print("sum = ",sum)
+
+print("mean = ",np.mean(arr))
+print("minimum = ",np.min(arr))
+print("maximum = ",np.max(arr))
+print("standard deviation = ",np.std(arr))
+print("variance = ",np.var(arr))
