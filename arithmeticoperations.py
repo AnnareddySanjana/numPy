63da215cf0f7d90562948178e9bea82f8d6ba0cb
Annareddy Sanjana <sanjana.12220788@lpu.in>
2025-04-27 13:29:11 +0530

    +,-,*,/,%,**,//

---
+++ arithmeticoperations.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+arr = np.array([5,2,7,3])
+add=(arr+4)
+print(add)
+
+print(arr-1)
+print(arr*2)
+print(arr/10)    //inclusion of decimals in result
+print(arr%2)
+print(arr**2)
+print(arr//2)    //exclusion of decimals in result
