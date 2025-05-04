57be25759bcc30cb731c7dd683f3dd0116497a84
Annareddy Sanjana <sanjana.12220788@lpu.in>
2025-05-04 14:56:32 +0530

    deleting the subarray

---
+++ deletion_in_2d.py
@@ -0,0 +1,11 @@
+import numpy as np 
+
+arr1 = np.array([1,2,3,4])
+
+new_arr = np.delete(arr1,3)
+print(new_arr)
+
+arr3 = np.array([[2,3],[4,5]])
+
+New_arr=np.delete(arr3, 1, axis =0)
+print(New_arr)
